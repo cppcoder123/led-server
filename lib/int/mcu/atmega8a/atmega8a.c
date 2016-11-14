e364c9f465371f3b8e09221cfa5b16e48e7510ce
cppcoder <cppcoder123@vfemail.net>
2016-11-14 22:06:20 +0300

    changing ucode a lot: one reply for msg

--- lib/int/mcu/atmega8a/atmega8a.c
+++ lib/int/mcu/atmega8a/atmega8a.c
@@ -15,17 +15,24 @@
 
 #include "libled/spi-message.h"
 
-// state bits
-//#define STATE_SPI_EYE_CATCHED           (1 << 0)
-#define STATE_SPI_READING               (1 << 1)
-#define STATE_SPI_WRITING               (1 << 2)
-#define STATE_SPI_BYTE_READ             (1 << 3)
-#define STATE_MATRIX_TRANSFER_STARTED   (1 << 4)
-#define STATE_MATRIX_RENDERED           (1 << 5)
-#define STATE_BRIGHTNESS                (1 << 6)
+
+// transfer state flags
+#define TRANSFER_IDLE    (1 << 0)
+#define TRANSFER_READING (1 << 1) // else writing
+#define TRANSFER_READY   (1 << 2) // byte has arrived or sent
+#define TRANSFER_MATRIX_STARTED  (1 << 3)
+#define TRANSFER_MATRIX_RENDERED (1 << 4)
+
+// parser state flags
+//#define STATE_SPI_READING               (1 << 1)
+//#define STATE_SPI_WRITING               (1 << 2)
+//#define STATE_SPI_BYTE_READ             (1 << 3)
+//#define STATE_MATRIX_TRANSFER_STARTED   (1 << 4)
+//#define STATE_MATRIX_RENDERED           (1 << 5)
+//#define STATE_BRIGHTNESS                (1 << 6)
 
 
-// max data array size
+// max matrix size
 #define MATRIX_CAPACITY SPI_MATRIX_CAPACITY
 
 // display size
@@ -33,20 +40,25 @@
 #define DISPLAY_CAPACITY (DISPLAY_HALF_CAPACITY * 2)
 
 //
-volatile uint8_t state_flag = 0;
+volatile uint8_t transfer_flag = 0;
+//
+volatile uint8_t transfer_byte = 0;
+//
+volatile uint8_t *transfer_buffer = 0;
+volatile uint8_t transfer_buffer_size = 0;
+volatile uint16_t transfer_index = 0;
 //
 volatile uint8_t *matrix = 0;
 volatile uint16_t matrix_size = 0;
-//
 //
 volatile uint8_t brightness = 0;
 //
 volatile uint8_t scroll_shift_delay = 0;
 //
 //
-volatile uint8_t parse_message_id = 0;
-volatile uint16_t parse_step = 0;
-volatile uint8_t parse_byte = 0;
+//volatile uint8_t parse_message_id = 0;
+//volatile uint16_t parse_step = 0;
+//volatile uint8_t parse_byte = 0;
 
 //
 // Interrupt handling
@@ -54,177 +66,351 @@ volatile uint8_t parse_byte = 0;
 
 ISR(SPI_STC_vect)
 {
-  if (state_flag & STATE_SPI_WRITING) {
-    state_flag &= ~STATE_SPI_WRITING;
-    // end of writing => we can read
-    state_flag |= STATE_SPI_READING;
-    // ingore end of send
-    return;
-  }
-  //
-  if (!(state_flag & STATE_SPI_READING))
+  if (transfer_flag & TRANSFER_IDLE)
     return;
 
-  //  if (!(state_flag & STATE_SPI_EYE_CATCHED)) {
-  //  if (SPDR == SPI_SLAVE_EYE_CATCH) {
-  //    state_flag &= ~STATE_SPI_READING;
-  //    state_flag |= STATE_SPI_EYE_CATCHED;
-  //  }
-  //  // ignore everything except eye-catch
-  //  return;
-  //}
-  
-  // we want to receive something
-  parse_byte = SPDR;
-  // ignore everything while parsing byte
-  state_flag &= ~STATE_SPI_READING;
-  state_flag |= STATE_SPI_BYTE_READ;
+  if (transfer_flag & TRANSFER_READING) {
+    transfer_byte = SPDR;
+    // don't send junk to master
+    SPDR = 0;
+    transfer_flag |= TRANSFER_READY;
+  } else {
+    ++transfer_index;
+    if (transfer_index < transfer_buffer_size)
+      SPDR = transfer_buffer[transfer_index];
+    else {
+      // write completed => start reading
+      transfer_flag |= TRANSFER_READING;
+    }
+  }
 }
+
+/*   SPDR = transfer_buffer[transfer_index]; */
+/*   if (transfer_index++ >) */
+/*   transfer_index */
+/*   SPDR = transfer_bubyte; */
+/* } */
+
+// ignore everything if we are not ready
+//}
+  
+//  if (transfer_flag & TRANSFER_READING) {
+//  } else {
+//    // we are writing
+//  }
+
+  
+//  if (state_flag & STATE_SPI_WRITING) {
+//    state_flag &= ~STATE_SPI_WRITING;
+//    // end of writing => we can read
+//    state_flag |= STATE_SPI_READING;
+//    // ingore end of send
+//    return;
+//  }
+//
+//  if (!(state_flag & STATE_SPI_READING))
+//    return;
+
+//  if (!(state_flag & STATE_SPI_EYE_CATCHED)) {
+//  if (SPDR == SPI_SLAVE_EYE_CATCH) {
+//    state_flag &= ~STATE_SPI_READING;
+//    state_flag |= STATE_SPI_EYE_CATCHED;
+//  }
+//  // ignore everything except eye-catch
+//  return;
+//}
+
+// we want to receive something
+//  parse_byte = SPDR;
+// // ignore everything while parsing byte
+//  state_flag &= ~STATE_SPI_READING;
+//  state_flag |= STATE_SPI_BYTE_READ;
+//}
 
 //
 // Handle incoming messages
 //
 
-uint8_t handle_start ()
-{
-  // fixme
-  return SPI_STATUS_OK;
-}
-
-uint8_t handle_stop ()
-{
-  // fixme
-  return SPI_STATUS_OK;
-}
-
 // message parse is complete
-void parse_complete ()
+void spi_parse_complete (uint8_t status)
 {
-  parse_message_id = SPI_MESSAGE_EMPTY;
-  parse_step = 0;
-  //state_flag &= ~STATE_SPI_EYE_CATCHED;
+  // ignore spi
+  transfer_flag |= TRANSFER_IDLE;
+  // fill reply buffer
+  transfer_buffer[0] = SPI_MESSAGE_MASTER_START;
+  transfer_buffer[1] = status;
+  transfer_buffer[3] = SPI_MESSAGE_MASTER_FINISH;
+  transfer_buffer_size = 3;
+  //
+  SPDR = transfer_buffer[0];
+  transfer_index = 0;
+
+  //
+  transfer_flag &= ~(TRANSFER_IDLE | TRANSFER_READING);
+}
+ 
+/*   transfer_byte = status; */
+/*   transfer_buffer_size = 0; */
+/*   // start writing */
+/*   transfer_flag &= ~TRANSFER_READING; */
+/* } */
+  
+/*   parse_message_id = SPI_MESSAGE_EMPTY; */
+/*   parse_step = 0; */
+/*   state_flag &= ~STATE_SPI_EYE_CATCHED; */
+/* } */
+
+void spi_start ()
+{
+  // fixme
+  spi_parse_complete (SPI_STATUS_OK);
 }
 
-uint8_t parse_matrix (uint8_t data)
+void spi_stop ()
 {
-  uint16_t index = 0;
-  
-  state_flag |= STATE_MATRIX_TRANSFER_STARTED;
-  switch (++parse_step) {
+  // fixme
+  spi_parse_complete (SPI_STATUS_OK);
+}
+
+void spi_handshake ()
+{
+  // we can reach this point only with 'SPI_MESSAGE_SLAVE_FINISH'
+  if (transfer_byte == SPI_MESSAGE_SLAVE_FINISH)
+    spi_parse_complete (SPI_STATUS_OK);
+  else
+    spi_parse_complete (SPI_STATUS_NO_FINISH);
+}
+
+void spi_matrix ()
+{
+  transfer_flag |= TRANSFER_MATRIX_STARTED;
+  switch (transfer_buffer_size) {
   case 2:
-    matrix_size = data;
+    matrix_size = transfer_byte;
+    ++transfer_buffer_size;
     break;
   case 3:
-    matrix_size += 255 * data;
+    matrix_size += 255 * transfer_byte;
+    ++transfer_buffer_size;
     break;
   case 4:
-    if (data != SPI_MATRIX_ARRAY_START)
-      return SPI_STATUS_NO_ARRAY_START;
-    break;
-  default:
-    index = parse_step - 5;
-    if (index > SPI_MATRIX_CAPACITY)
-      return SPI_STATUS_TOO_LONG_MATRIX;
-    if (index == matrix_size) {
-      if (data != SPI_MATRIX_ARRAY_FINISH)
-        return SPI_STATUS_NO_ARRAY_FINISH;
-      // message receive is completed
-      state_flag &= ~STATE_MATRIX_TRANSFER_STARTED;
-      parse_complete ();
+    if (transfer_byte != SPI_MATRIX_ARRAY_START) {
+      spi_parse_complete (SPI_STATUS_NO_ARRAY_START);
     } else {
-      matrix[index] = data;
+      transfer_index = 0;
+      ++transfer_buffer_size;
     }
     break;
+  default:
+    if (transfer_index < matrix_size) {
+      matrix[transfer_index++] = transfer_byte;
+    } else if (transfer_index == matrix_size) {
+      if (transfer_byte == SPI_MESSAGE_SLAVE_FINISH)
+        spi_parse_complete (SPI_STATUS_OK);
+      else
+        spi_parse_complete (SPI_STATUS_NO_FINISH);
+      transfer_flag &= TRANSFER_MATRIX_STARTED;
+    } else {
+      spi_parse_complete (SPI_STATUS_PARSE_ERROR);
+      transfer_flag &= TRANSFER_MATRIX_STARTED;
+    }
   }
-
-  return SPI_STATUS_OK;
 }
 
-uint8_t parse_delay (uint8_t data)
+    
+/*     index = parse_step - 5; */
+/*     if (index > SPI_MATRIX_CAPACITY) */
+/*       return SPI_STATUS_TOO_LONG_MATRIX; */
+/*     if (index == matrix_size) { */
+/*       if (data != SPI_MATRIX_ARRAY_FINISH) */
+/*         return SPI_STATUS_NO_ARRAY_FINISH; */
+/*       // message receive is completed */
+/*       state_flag &= ~STATE_MATRIX_TRANSFER_STARTED; */
+/*       parse_complete (); */
+/*     } else { */
+/*       matrix[index] = data; */
+/*     } */
+/*     break; */
+/*   } */
+
+/*   return SPI_STATUS_OK; */
+/* } */
+
+void spi_delay ()
 {
-  parse_step++;
-  if (parse_step == 2) {
-    if (data != SPI_DELAY_SCROLL_SHIFT)
-      return SPI_STATUS_UNKNOWN_DELAY_ID;
-  } else if (parse_step == 3) {
-    scroll_shift_delay = data;
-    parse_complete ();
+  switch (transfer_buffer_size++) {
+  case 2:
+    if (transfer_byte != SPI_DELAY_SCROLL_SHIFT)
+      spi_parse_complete (SPI_STATUS_UNKNOWN_DELAY_ID);
+    break;
+  case 3:
+    scroll_shift_delay = transfer_byte;
+    break;
+  case 4:
+    spi_parse_complete
+      ((transfer_byte == SPI_MESSAGE_SLAVE_FINISH)
+       ? SPI_STATUS_OK : SPI_STATUS_NO_FINISH);
+      break;
+  default:
+    spi_parse_complete (SPI_STATUS_PARSE_ERROR);
+    break;
   }
-
-  return SPI_STATUS_OK;
 }
+/*   parse_step++; */
+/*   if (parse_step == 2) { */
+/*     if (data != SPI_DELAY_SCROLL_SHIFT) */
+/*       return SPI_STATUS_UNKNOWN_DELAY_ID; */
+/*   } else if (parse_step == 3) { */
+/*     scroll_shift_delay = data; */
+/*     parse_complete (); */
+/*   } */
 
-uint8_t parse_brightness (uint8_t data)
+/*   return SPI_STATUS_OK; */
+/* } */
+
+void spi_brightness ()
 {
-  // 2 steps parse, so data should be brightness
-  if ((data < SPI_BRIGHTNESS_MIN)
-      || (data > SPI_BRIGHTNESS_MAX))
-    return SPI_STATUS_BRIGHTNESS_OUT_OF_RANGE;
+  switch (transfer_buffer_size++) {
+  case 2:
+    transfer_buffer[transfer_buffer_size] = transfer_byte;
+    break;
+  case 3:
+    if ((transfer_buffer[2] < SPI_BRIGHTNESS_MIN)
+        || (transfer_buffer[2] > SPI_BRIGHTNESS_MAX))
+      spi_parse_complete (SPI_STATUS_BRIGHTNESS_OUT_OF_RANGE);
+    brightness = transfer_buffer[2];
+    // fixme : send brightness to ht1632c here
+    if (transfer_byte == SPI_MESSAGE_SLAVE_FINISH)
+      spi_parse_complete (SPI_STATUS_OK);
+    else
+      spi_parse_complete (SPI_STATUS_NO_FINISH);
+    break;
+  default:
+    spi_parse_complete (SPI_STATUS_PARSE_ERROR);
+    break;
+  }
+}
+/*   if ((transfer_byte < SPI_BRIGHTNESS_MIN) */
+/*       || (transfer_byte > SPI_BRIGHTNESS_MAX)) */
+/*     spi_parse_complete (SPI_STATUS_BRIGHTNESS_OUT_OF_RANGE); */
 
-  brightness = data;
-  state_flag |= STATE_BRIGHTNESS;
+/*   spi_parse_complete (SPI_STATUS_OK); */
+
+
   
-  parse_complete ();
+/*   switch (transfer_buffer_size++) { */
+/*   case 2: */
+/*     break */
+/*   } */
+  
+/*   // 2 steps parse, so data should be brightness */
+/*   if ((data < SPI_BRIGHTNESS_MIN) */
+/*       || (data > SPI_BRIGHTNESS_MAX)) */
+/*     return SPI_STATUS_BRIGHTNESS_OUT_OF_RANGE; */
 
-  return SPI_STATUS_OK;
-}
+/*   brightness = data; */
+/*   state_flag |= STATE_BRIGHTNESS; */
+  
+/*   parse_complete (); */
 
-uint8_t spi_read (uint8_t data)
+/*   return SPI_STATUS_OK; */
+/* } */
+
+void spi_read ()
 {
-  uint8_t status = SPI_STATUS_OK;
-  if (parse_message_id == SPI_MESSAGE_EMPTY) {
-    if ((data >= SPI_MESSAGE_MIN)
-        && (data < SPI_MESSAGE_MAX)) {
-      // handle one byte messages right here
-      switch (data) {
-      case SPI_MESSAGE_START:
-        status = handle_start ();
-        break;
-      case SPI_MESSAGE_STOP:
-        status = handle_stop ();
-        break;
-      case SPI_MESSAGE_HANDSHAKE:
-        // status initialized with OK
-        break;
-      default:
-        parse_message_id = data;
-        parse_step = 1;
-        break;
-      }
+  if (transfer_buffer_size == 0) {
+    if (transfer_byte == SPI_MESSAGE_SLAVE_START)
+      transfer_buffer[transfer_buffer_size++] = SPI_MESSAGE_SLAVE_START;
+  } else if (transfer_buffer_size == 1) {
+    // we expect msg id now
+    if ((transfer_byte < SPI_MESSAGE_MIN)
+        || (transfer_byte >= SPI_MESSAGE_MAX)) {
+      spi_parse_complete (SPI_STATUS_MESSAGE_ID_UNKNOWN);
     } else {
-      status = SPI_STATUS_MESSAGE_ID_UNKNOWN;
+      transfer_buffer[transfer_buffer_size++] = transfer_byte;
     }
-
-    parse_complete ();
-    return status;
+  } else {
+    switch (transfer_buffer[1]) {
+    case SPI_MESSAGE_START:
+      spi_start ();
+      break;
+    case SPI_MESSAGE_STOP:
+      spi_stop ();
+      break;
+    case SPI_MESSAGE_HANDSHAKE:
+      spi_handshake ();
+      break;
+    case SPI_MESSAGE_MATRIX:
+      spi_matrix ();
+      break;
+    case SPI_MESSAGE_DELAY:
+      spi_delay ();
+      break;
+    case SPI_MESSAGE_BRIGHTNESS:
+      spi_brightness ();
+      break;
+    default:
+      // we shouldn't reach this point
+      spi_parse_complete (SPI_STATUS_LONG_MESSAGE_ID_UNKNOWN);
+      break;
+    }
   }
-
-  // not a first step in message parsing
-  switch (parse_message_id) {
-  case SPI_MESSAGE_MATRIX:
-    status = parse_matrix (data);
-    break;
-  case SPI_MESSAGE_DELAY:
-    status = parse_delay (data);
-    break;
-  case SPI_MESSAGE_BRIGHTNESS:
-    status = parse_brightness (data);
-    break;
-  default:
-    // ! we should not reach this point
-    status = SPI_STATUS_LONG_MESSAGE_ID_UNKNOWN;
-    break;
-  }
-
-  return status;
 }
 
-void spi_write (uint8_t data)
-{
-  state_flag &= ~STATE_SPI_BYTE_READ;
-  state_flag |= STATE_SPI_WRITING;
-  SPDR = data;
-}
+/*   uint8_t status = SPI_STATUS_OK; */
+/*   if (parse_message_id == SPI_MESSAGE_EMPTY) { */
+/*     if ((data >= SPI_MESSAGE_MIN) */
+/*         && (data < SPI_MESSAGE_MAX)) { */
+/*       // handle one byte messages right here */
+/*       switch (data) { */
+/*       case SPI_MESSAGE_START: */
+/*         status = handle_start (); */
+/*         break; */
+/*       case SPI_MESSAGE_STOP: */
+/*         status = handle_stop (); */
+/*         break; */
+/*       case SPI_MESSAGE_HANDSHAKE: */
+/*         // status initialized with OK */
+/*         break; */
+/*       default: */
+/*         parse_message_id = data; */
+/*         parse_step = 1; */
+/*         break; */
+/*       } */
+/*     } else { */
+/*       status = SPI_STATUS_MESSAGE_ID_UNKNOWN; */
+/*     } */
+
+/*     parse_complete (); */
+/*     return status; */
+/*   } */
+
+/*   // not a first step in message parsing */
+/*   switch (parse_message_id) { */
+/*   case SPI_MESSAGE_MATRIX: */
+/*     status = parse_matrix (data); */
+/*     break; */
+/*   case SPI_MESSAGE_DELAY: */
+/*     status = parse_delay (data); */
+/*     break; */
+/*   case SPI_MESSAGE_BRIGHTNESS: */
+/*     status = parse_brightness (data); */
+/*     break; */
+/*   default: */
+/*     // ! we should not reach this point */
+/*     status = SPI_STATUS_LONG_MESSAGE_ID_UNKNOWN; */
+/*     break; */
+/*   } */
+
+/*   return status; */
+/* } */
+
+/* void spi_write (uint8_t data) */
+/* { */
+/*   state_flag &= ~STATE_SPI_BYTE_READ; */
+/*   state_flag |= STATE_SPI_WRITING; */
+/*   SPDR = data; */
+/* } */
 
 //
 // Init code
@@ -263,20 +449,21 @@ void hw_init ()
 
 void sw_init ()
 {
-  state_flag = 0;
+  transfer_flag = 0;
+  //
+  transfer_byte = 0;
+  //
+  transfer_buffer = (uint8_t*) calloc (SPI_MESSAGE_MAX_LENGTH, sizeof (uint8_t));
+  transfer_buffer_size = 0;
+  //
+  transfer_index = 0;
   //
   matrix = (uint8_t*) calloc (MATRIX_CAPACITY, sizeof (uint8_t));
-  //
   matrix_size = 0;
   //
   brightness = SPI_BRIGHTNESS_MAX;
   //
-  scroll_shift_delay = 10;      /* fixme : ? */
-  //
-  //
-  parse_message_id = SPI_MESSAGE_EMPTY;
-  parse_step = 0;
-  parse_byte = 0;
+  scroll_shift_delay = 30;      /* fixme : ? */
 }
 
 //
@@ -313,7 +500,7 @@ void clear ()
 
 uint8_t is_matrix_consistent ()
 {
-  return (state_flag & STATE_MATRIX_TRANSFER_STARTED) ? 0 : 1;
+  return (transfer_flag & TRANSFER_MATRIX_STARTED) ? 0 : 1;
 }
 
 uint8_t get_column (uint16_t index)
@@ -327,7 +514,7 @@ uint8_t get_column (uint16_t index)
 
 void render_matrix_fixed ()
 {
-  if (state_flag & STATE_MATRIX_RENDERED)
+  if (transfer_flag & TRANSFER_MATRIX_RENDERED)
     return;
   
   clear ();
@@ -341,7 +528,7 @@ void render_matrix_fixed ()
   }
 
   // don't refresh static info
-  state_flag |= STATE_MATRIX_RENDERED;
+  transfer_flag |= TRANSFER_MATRIX_RENDERED;
 }
 
 void render_matrix_scroll ()
@@ -417,14 +604,21 @@ int main ()
     //if (state_flag & STATE_SPI_EYE_CATCHED) {
     //  spi_write (SPI_STATUS_OK);
     //}
-    if (state_flag & STATE_SPI_BYTE_READ) {
-      spi_write (spi_read (parse_byte));
-      state_flag &= ~STATE_SPI_BYTE_READ;
-    }
+    //    if (state_flag & STATE_SPI_BYTE_READ) {
+    //      spi_write (spi_read (parse_byte));
+    //      state_flag &= ~STATE_SPI_BYTE_READ;
+    //    }
     //
-    if (state_flag & STATE_BRIGHTNESS) {
-      change_brightness ();
-      state_flag &= ~STATE_BRIGHTNESS;
+    //    if (state_flag & STATE_BRIGHTNESS) {
+    //      change_brightness ();
+    //      state_flag &= ~STATE_BRIGHTNESS;
+    //    }
+    //
+    if (transfer_flag & TRANSFER_READY) {
+      if (transfer_flag & TRANSFER_READING) {
+        spi_read ();
+        transfer_flag &= ~TRANSFER_READY;
+      } 
     }
     //
     if (is_matrix_consistent ()) {
