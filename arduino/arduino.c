ba06c86b5e8586b5ad4983c4175b1e35dfcd08dc
cppcoder <cppcoder123@vfemail.net>
2018-05-28 22:17:56 +0200

    arduino: Write first lines of code

--- arduino/arduino.c
+++ arduino/arduino.c
@@ -6,6 +6,10 @@
  *
  */
 
+#include <stdint.h>
+
+#include "device-id.h"
+
 enum {
   MATRIX_READ_STARTED = (1 << 0),
   MATRIX_READ_FINISHED = (1 << 1),
@@ -18,3 +22,41 @@ enum {
   /*First matrix message raises the bit, and last drops*/
   MATRIX_THROTTLE = (1 << 4)
 };
+
+volatile uint8_t matrix_flag = 0;
+
+/*
+ * It is ok to read if copy is finished or read started and not finished
+ * and we are not in throttling
+ */
+#define MATRIX_OK_TO_READ                                               \
+  ((matrix_flag & MATRIX_THROTTLE == 0)                                 \
+   && (((matrix_flag & MATRIX_READ_STARTED != 0)                        \
+        && (matrix_flag & MATRIX_READ_FINISHED == 0))                   \
+       || (matrix_flag & MATRIX_COPY_FINISHED != 0)))
+
+/*
+ * It is ok to copy if matrix read started & finished but copy is not finished
+ *
+ */
+#define MATRIX_OK_TO_COPY                                       \
+  ((matrix_flag & MATRIX_READ_MASK == MATRIX_READ_MASK)         \
+   && (matrix_flag & MATRIX_COPY_FINISHED == 0))
+
+/*
+ * Matrix data buffer
+ */
+struct matrix_data_t
+{
+  uint8_t data[ID_MAX_MATRIX_SIZE];
+  uint16_t size;                /* actual data size */
+};
+
+volatile struct matrix_data_t matrix_read_buffer;
+volatile struct matrix_data_t matrix_render_buffer;
+
+int main ()
+{
+
+  return 0;
+}
