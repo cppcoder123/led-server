f5ce1483069d65cd4f0a0e681d7ca365fa1481f0
cppcoder <cppcoder123@vfemail.net>
2018-05-29 22:06:12 +0200

    arduino: Make skeleton

--- arduino/arduino.c
+++ arduino/arduino.c
@@ -15,9 +15,7 @@ enum {
   MATRIX_READ_FINISHED = (1 << 1),
   MATRIX_READ_MASK = (MATRIX_READ_STARTED | MATRIX_READ_FINISHED),
   /**/
-  MATRIX_COPY_STARTED = (1 << 2),
-  MATRIX_COPY_FINISHED = (1 << 3),
-  MATRIX_COPY_MASK = (MATRIX_COPY_STARTED | MATRIX_COPY_FINISHED),
+  MATRIX_COPY_FINISHED = (1 << 2),
   /**/
   /*First matrix message raises the bit, and last drops*/
   MATRIX_THROTTLE = (1 << 4)
@@ -40,8 +38,8 @@ volatile uint8_t matrix_flag = 0;
  *
  */
 #define MATRIX_OK_TO_COPY                                       \
-  ((matrix_flag & MATRIX_READ_MASK == MATRIX_READ_MASK)         \
-   && (matrix_flag & MATRIX_COPY_FINISHED == 0))
+  (((matrix_flag & MATRIX_READ_MASK) == MATRIX_READ_MASK)       \
+   && ((matrix_flag & MATRIX_COPY_FINISHED) == 0))
 
 /*
  * Matrix data buffer
@@ -55,8 +53,83 @@ struct matrix_data_t
 volatile struct matrix_data_t matrix_read_buffer;
 volatile struct matrix_data_t matrix_render_buffer;
 
+#define MATRIX_PHYSICAL_SIZE 32
+
+#define MATRIX_IS_SHORT (matrix_render_buffer.size == MATRIX_PHYSICAL_SIZE)
+#define MATRIX_IS_LONG (matrix_render_buffer.size > MATRIX_PHYSICAL_SIZE)
+
+void hardware_init ()
+{
+  /* fixme */
+}
+
+void software_init ()
+{
+  /*assume last cycle was OK*/
+  matrix_flag = MATRIX_READ_MASK | MATRIX_COPY_FINISHED;
+}
+
+void hw_sw_test ()
+{
+  /* fixme*/
+}
+
+void handle_io ()
+{
+  /* fixme */
+}
+
+void copy_matrix ()
+{
+  uint8_t left = 0;
+  uint8_t right = 0;
+
+  if (matrix_read_buffer.size < MATRIX_PHYSICAL_SIZE) {
+    uint8_t delta = MATRIX_PHYSICAL_SIZE - matrix_read_buffer.size;
+    left = delta / 2;
+    right = delta - left;
+  }
+
+  uint8_t i = 0;
+  for (i = 0; i < left; ++i) {
+    matrix_render_buffer.data[i] = 0;
+  }
+  for (i = 0; i < matrix_read_buffer.size; ++i) {
+    matrix_render_buffer.data[i + left] = matrix_read_buffer.data[i];
+  }
+  for (i = 0; i < right; ++i) {
+    matrix_render_buffer.data[left + matrix_read_buffer.size + i] = 0;
+  }
+
+  matrix_flag |= MATRIX_COPY_FINISHED;
+}
+
+void render_stable ()
+{
+  /* fixme */
+}
+
+void render_rolling ()
+{
+  /* fixme */
+}
+
 int main ()
 {
+  hardware_init ();
+  software_init ();
+  hw_sw_test ();
 
+  while (1) {
+    handle_io ();
+    if (MATRIX_OK_TO_COPY)
+      copy_matrix ();
+    if (MATRIX_IS_SHORT) {
+      render_stable ();
+    } else if (MATRIX_IS_LONG) {
+      render_rolling ();
+    }
+  }
+  
   return 0;
 }
