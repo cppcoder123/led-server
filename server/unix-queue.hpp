9a28cf165e58df083418c47dba26fab75dc4cfcd
cppcoder123 <cppcoder123@vfemail.net>
2019-09-30 07:45:56 +0200

    new-queue: Compilation test

--- server/unix-queue.hpp
+++ server/unix-queue.hpp
@@ -4,10 +4,10 @@
 #ifndef UNIX_QUEUE_HPP
 #define UNIX_QUEUE_HPP
 
-#include <condition_variable>
-#include <functional>           // std::reference_wrapper
+//#include <condition_variable>
+//#include <functional>           // std::reference_wrapper
 #include <memory>
-#include <mutex>
+//#include <mutex>
 
 #include "unix/condition-queue.hpp"
 
@@ -20,10 +20,13 @@ namespace led_d
   using unix_msg_ptr_t = std::unique_ptr<unix_msg_t>;
   using unix_queue_t = mutex::queue_t<unix_msg_ptr_t>;
 
-  using unix_queue_a_t
-  = unix::condition_queue_t<unix_msg_ptr_t,
-                            std::reference_wrapper<std::mutex>,
-                            std::reference_wrapper<std::condition_variable>>;
+  // using unix_queue_t
+  // = unix::condition_queue_t<unix_msg_ptr_t,
+  //                           std::reference_wrapper<std::mutex>,
+  //                           std::reference_wrapper<std::condition_variable>>;
+  // using unix_queue_t
+  // = unix::condition_queue_t<unix_msg_ptr_t,
+  //                           std::mutex, std::condition_variable>;
 
 } // namespace led_d
 
