5ac4c585daf1ddddd4e73861ede39630334d12b0
cppcoder123 <cppcoder123@vfemail.net>
2019-10-12 09:13:09 +0200

    avr: Implement first var of keyboard module

--- avr/avr-2560/keyboard.c
+++ avr/avr-2560/keyboard.c
@@ -1,8 +1,12 @@
 /*
  *
  */
+
+#define F_CPU 4000000UL
+
 #include <avr/interrupt.h>
 #include <stdint.h>
+#include <util/delay.h>
 
 #include "mcu/constant.h"
 
@@ -13,22 +17,26 @@
 
 #define BOARD_ERROR_SHIFT_100 100
 
-#define BOARD_WAKEUP 0x01
-#define BOARD_DETECT_SUCCESS 0  /* fixme */
-
-#define BOARD_REG_1 0x1C
-#define BOARD_REG_2 0x40
-#define BOARD_REG_STATUS 0x1A
+#define BOARD_REG_CONTROL_1 0x1C
+#define BOARD_REG_CONTROL_2 0x40
+#define BOARD_REG_GENERAL_STATUS 0x1A
+#define BOARD_REG_CHANNEL_STATUS 0x1B
 #define BOARD_REG_DATA 0x19
 #define BOARD_REG_OFFCAL_CYCLE 0x22
 
-#define BOARD_VALUE_0X80 0x80
-#define BOARD_VALUE_0X01 0x01
-#define BOARD_VALUE_0X1C 0x1C
+#define BOARD_VALUE_WAKEUP 0x01
+#define BOARD_VALUE_CALIBRATE 0x80
+#define BOARD_VALUE_CALIBRATE_PARAM 0x01
+#define BOARD_VALUE_CALIBRATE_APPLY 0x87
+/* it doesn't matter what we have for x */
+/* 0xxx x000 (negate=>) 1xxx x111 => 1000 0111 => 0x87*/
+#define BOARD_VALUE_CALIBRATED_MASK 0x87
+/* MSB and LSB to 1 */
+#define BOARD_VALUE_GENERAL_STATUS_MASK 0x81
+/* all bits should be zero */
+#define BOARD_VALUE_CHANNEL_STATUS_MASK 0xFF
 
-// state of our buttons
-static volatile uint8_t state = 0;
-static uint8_t prev_state = 0;
+#define PORT_RESET PORTC0
 
 /* mode of operation */
 enum {
@@ -41,33 +49,44 @@ enum {
   mode_calibrate_apply,         /* apply calibration values */
   mode_wakeup_write,            /* write wakeup command */
   mode_wakeup_wait,             /* wait the interrupt */
-  mode_status_read,             /* finger detection status read */
-  mode_status_check,            /* finger detection status check */
-  mode_data_read,               /* finger detection data read */
-  mode_data_handle,             /* finger detection data handle */
+  mode_general_status_read,     /* measurement general status read */
+  mode_general_status_check,    /* measurement general status check */
+  mode_channel_status_read,     /* measurement channel status read */
+  mode_channel_status_check,    /* measurement channel status check */
+  mode_data_read,               /* measurement data read */
+  mode_data_handle,             /* measurement data handle */
   mode_idle,                    /* ready for next cycle */
   mode_last = mode_idle         /* last mode */
 };
-static volatile uint8_t mode = mode_reset;
-
+static uint8_t mode = mode_reset;
 static volatile uint8_t mode_advance = 0;
+
+/* state of our buttons */
+static uint8_t state = 0;
+
+/* read register value */
+static uint8_t read_value = 0;
 
 static void board_reset ()
 {
-  //fixme
-  // make low pulse here on reset line
+  /* set portc 0th bit to 0, then to 1 */
+  PORTC &= ~(1 << PORT_RESET);
+  _delay_us (10);
+  PORTC |= (1 << PORT_RESET);
 }
 
 void keyboard_init ()
 {
-  state = 0;
-  prev_state = 0;
-
-  // fixme:
-  // init ext interrupt request line here
-
   mode = mode_reset;
   mode_advance = 0;
+
+  state = 0;
+
+  read_value = 0;
+
+  EICRA |= (1 << ISC20) | (1 << ISC21); /* rising edge */
+  EIMSK |= (1 << INT2);                 /* enable int2 interrupt */
+
   board_reset ();
 }
 
@@ -93,7 +112,7 @@ static void read_completed (uint8_t status, uint8_t data)
     mode = mode_error;
   }
 
-  //fixme: store data in global var
+  read_value = data;
   
   mode_advance = 1;
 }
@@ -103,21 +122,19 @@ static void read_reg (uint8_t reg)
   twi_read_reg (reg, read_completed);
 }
 
-static uint8_t is_calibrated ()
+static uint8_t check_status (uint8_t mask)
 {
-  // fixme
-  return 1;
+  return ((read_value & mask) == 0) ? 1 : 0;
 }
 
-static uint8_t good_status ()
+static void handle_data (uint8_t new_state)
 {
-  // fixme
-  return 1;
-}
+  if (new_state == state)
+    return;
 
-static void handle_data ()
-{
-  // fixme
+  encode_msg_2 (MSG_ID_BUTTON, SERIAL_ID_TO_IGNORE, new_state, state);
+
+  state = new_state;
 }
 
 void keyboard_try ()
@@ -140,33 +157,42 @@ void keyboard_try ()
     mode_advance = 1;           /* nothing to do */
     break;
   case mode_calibrate_start:
-    write_reg (BOARD_REG_1, BOARD_VALUE_0X80);
+    write_reg (BOARD_REG_CONTROL_1, BOARD_VALUE_CALIBRATE);
     break;
   case mode_calibrate_wait_start:
-    read_reg (BOARD_REG_1);
+    read_reg (BOARD_REG_CONTROL_1);
     break;
   case mode_calibrate_wait_check:
-    mode_advance = 1;
-    if (is_calibrated () == 0)
+    if (check_status (BOARD_VALUE_CALIBRATED_MASK) == 0)
       mode -= 2;                /* one step backward */
     break;
   case mode_calibrate_param:
-    write_reg (BOARD_REG_OFFCAL_CYCLE, BOARD_VALUE_0X01);
+    write_reg (BOARD_REG_OFFCAL_CYCLE, BOARD_VALUE_CALIBRATE_PARAM);
     break;
   case mode_calibrate_apply:
-    write_reg (BOARD_REG_1, BOARD_VALUE_0X1C);
+    write_reg (BOARD_REG_CONTROL_1, BOARD_VALUE_CALIBRATE_APPLY);
     break;
   case mode_wakeup_write:
-    write_reg (BOARD_REG_2, BOARD_WAKEUP);
+    write_reg (BOARD_REG_CONTROL_2, BOARD_VALUE_WAKEUP);
     break;
   case mode_wakeup_wait:
     // waiting for interrupt here
+    mode_advance = 1;
     break;
-  case mode_status_read:
-    read_reg (BOARD_REG_STATUS);
+  case mode_general_status_read:
+    read_reg (BOARD_REG_GENERAL_STATUS);
     break;
-  case mode_status_check:
-    if (good_status () == 0) {
+  case mode_general_status_check:
+    if (check_status (BOARD_VALUE_GENERAL_STATUS_MASK) == 0) {
+      mode = mode_error;
+    }
+    mode_advance = 1;
+    break;
+  case mode_channel_status_read:
+    read_reg (BOARD_REG_CHANNEL_STATUS);
+    break;
+  case mode_channel_status_check:
+    if (check_status (BOARD_VALUE_CHANNEL_STATUS_MASK) == 0) {
       mode = mode_error;
     }
     mode_advance = 1;
@@ -176,7 +202,7 @@ void keyboard_try ()
     break;
   case mode_data_handle:
     mode_advance = 1;
-    handle_data ();
+    handle_data (read_value);
     break;
   case mode_idle:
     mode_advance = 1;
@@ -188,7 +214,6 @@ void keyboard_try ()
     encode_msg_1 (MSG_ID_BOARD_HANDLE_ERROR, SERIAL_ID_TO_IGNORE, mode);
     break;
   }
-
 }
 
 /* handle external interrupt from board */
