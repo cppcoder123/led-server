d1bab4c94bbc1fa26902d36607971a7af6335f73
cppcoder123 <cppcoder123@vfemail.net>
2019-12-17 22:00:50 +0100

    pi: Add more code to bash-out

--- src/pi/bash-out.cpp
+++ src/pi/bash-out.cpp
@@ -7,5 +7,27 @@
 namespace led_d
 {
 
+  bash_out_t::bash_out_t (status_queue_t &status_queue)
+    : m_status_queue (status_queue),
+      m_go (true)
+  {
+  }
+
+  void bash_out_t::start ()
+  {
+    while (m_go.load () == true) {
+      auto command = m_command_queue.pop<true>();
+      if (!command)
+        continue;
+      invoke_command (*command);
+    }
+  }
+
+  void bash_out_t::stop ()
+  {
+    m_go.store (false);
+
+    m_command_queue.notify_one<true> ();
+  }
   
 } // led_d
--- src/pi/bash-out.hpp
+++ src/pi/bash-out.hpp
@@ -5,6 +5,10 @@
 #ifndef BASH_OUT_HPP
 #define BASH_OUT_HPP
 
+#include <atomic>
+#include <condition_variable>
+#include <mutex>
+
 #include "command-queue.hpp"
 #include "status-queue.hpp"
 
@@ -15,7 +19,7 @@ namespace led_d
   {
   public:
     bash_out_t (status_queue_t &status_queue);
-    ~bash_out_t ();
+    ~bash_out_t () = default;
 
     void start ();
     void stop ();
@@ -24,8 +28,13 @@ namespace led_d
 
   private:
 
-    command_queue_t m_command_queue;
+    void invoke_command (command_t command);
+
     status_queue_t &m_status_queue;
+
+    command_queue_t m_command_queue;
+
+    std::atomic_bool m_go;
 
   };
 
