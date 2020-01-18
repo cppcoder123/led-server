16a03f2fbc4d9f4a631f162e4c81697766de3ee3
cppcoder123 <cppcoder123@vfemail.net>
2020-01-18 20:51:11 +0100

    avr: Polish buzz module

--- src/avr/buzz.c
+++ src/avr/buzz.c
@@ -13,14 +13,7 @@
 #define SOUND_COUNTER COUNTER_0
 #define SOUND_PRESCALER COUNTER_PRESCALER_1024
 
-#define SOUND_SIZE 4
-#define PAUSE_SIZE 2
-
-enum {
-  EOF,
-  PAUSE,
-  SOUND,
-};
+#define SOUND_SIZE 3
 
 static uint8_t in_progress = 0;
 static volatile uint8_t repeat_counter = 0;
@@ -31,7 +24,7 @@ static volatile uint8_t melody_position = 0;
 
 static void handle_sound ();
 
-void buzz_init()
+void buzz_init ()
 {
   in_progress = 0;
   repeat_counter = 0;
@@ -40,36 +33,27 @@ void buzz_init()
   melody_position = 0;
 }
 
-/* it returns either SOUND, PAUSE or EOF */
-static uint8_t get_sound(uint8_t *duration, uint8_t *pitch, uint8_t *volume)
+/* it returns 0 in case of failure */
+static uint8_t get_sound (uint8_t *duration, uint8_t *pitch, uint8_t *volume)
 {
-  if (melody_position >= buf_size(&melody)) {
+  if (melody_position + SOUND_SIZE > buf_size (&melody)) {
     if (++repeat_counter >= repeat_limit)
-      return EOF;
+      return 0;
     melody_position = 0;
   }
 
-  uint8_t what = 0;
-  if ((buf_byte_get(&melody, melody_position++, &what) == 0)
-      || ((what != SOUND)
-          && (what != PAUSE))) /* either sound or silence */
-    return EOF;                /* internal error */
+  if ((buf_byte_get (&melody, melody_position++, pitch) == 0)
+      || (buf_byte_get (&melody, melody_position++, volume) == 0)
+      || (buf_byte_get (&melody, melody_position++, duration) == 0))
+    return 0;
 
-  if (what == SOUND) {
-    if ((buf_byte_get(&melody, melody_position++, pitch) == 0)
-        || (buf_byte_get(&melody, melody_position++, volume) == 0))
-      return EOF;               /* internal error */
-  }
-  if (buf_byte_get(&melody, melody_position++, duration) == 0)
-    return EOF;                 /* internal error */
-
-  return what;
+  return 1;
 }
 
-static void set_pitch_volume(uint8_t pitch, uint8_t volume)
+static void set_pitch_volume (uint8_t pitch, uint8_t volume)
 {
   /* fixme */
-  counter_enable(SOUND_COUNTER, SOUND_PRESCALER);
+  counter_enable (SOUND_COUNTER, SOUND_PRESCALER);
 }
 
 static void duration_callback ()
@@ -80,31 +64,20 @@ static void duration_callback ()
   handle_sound ();
 }
 
-/* static void set_duration(uint16_t duration) */
-/* { */
-/*   invoke_enable (INVOKE_ID_BUZZ, duration, duration_callback); */
-/* } */
-
 static void handle_sound ()
 {
   uint8_t duration = 0, pitch = 0, volume = 0;
 
-  switch (get_sound (&duration, &pitch, &volume)) {
-  case EOF:
+  if (get_sound (&duration, &pitch, &volume) == 0)
     return;
-  case SOUND:
-    set_pitch_volume(pitch, volume);
-    /* intentionally no break here */
-  case PAUSE:
-    /* set_duration(duration); */
-    invoke_enable (INVOKE_ID_BUZZ, duration, duration_callback);
-    break;
-  default:
-    break;
-  }
+
+  if ((pitch != 0) && (volume != 0))
+    set_pitch_volume (pitch, volume);
+
+  invoke_enable (INVOKE_ID_BUZZ, duration, duration_callback);
 }
 
-void buzz_start()
+void buzz_start ()
 {
   if (in_progress != 0)
     return;
@@ -114,58 +87,46 @@ void buzz_start()
   handle_sound ();
 }
 
-void buzz_stop()
+void buzz_stop ()
 {
   if (in_progress == 0)
     return;
 
-  counter_disable(SOUND_COUNTER);
-  /* counter_disable(DURATION_COUNTER); */
+  counter_disable (SOUND_COUNTER);
+  /* counter_disable (DURATION_COUNTER); */
 }
 
-void buzz_clear()
+void buzz_clear ()
 {
   if (in_progress != 0)
     return;
 
-  buf_clear(&melody);
+  buf_clear (&melody);
   repeat_limit = 0;
 
   melody_position = 0;
 }
 
-/* static void split_16_bit(uint16_t src, uint8_t *high, uint8_t *low) */
-/* { */
-/*   *low = (uint8_t) (src & 0xFF); */
-/*   *high = (uint8_t) (src >> 8); */
-/* } */
-
-uint8_t buzz_add_sound(uint8_t pitch, uint8_t volume, uint8_t duration)
+uint8_t buzz_add_sound (uint8_t pitch, uint8_t volume, uint8_t duration)
 {
-  if (buf_space(&melody) < SOUND_SIZE)
+  if (buf_space (&melody) < SOUND_SIZE)
     return 0;
 
   if (volume > pitch)
     volume = pitch;
 
-  return ((buf_byte_fill(&melody, SOUND) == 0)
-          || (buf_byte_fill(&melody, pitch) == 0)
-          || (buf_byte_fill(&melody, volume) == 0)
-          || (buf_byte_fill(&melody, duration) == 0))
+  return ((buf_byte_fill (&melody, pitch) == 0)
+          || (buf_byte_fill (&melody, volume) == 0)
+          || (buf_byte_fill (&melody, duration) == 0))
     ? 0 : 1;
 }
 
-uint8_t buzz_add_pause(uint8_t duration)
+uint8_t buzz_add_pause (uint8_t duration)
 {
-  if (buf_space(&melody) < PAUSE_SIZE)
-    return 0;
-
-  return ((buf_byte_fill(&melody, PAUSE) == 0)
-          || (buf_byte_fill(&melody, duration) == 0))
-    ? 0 : 1;
+  return buzz_add_sound (0, 0, duration);
 }
 
-void buzz_repeat(uint8_t num)
+void buzz_repeat (uint8_t num)
 {
   repeat_limit = num;
 }
--- src/avr/buzz.h
+++ src/avr/buzz.h
@@ -14,7 +14,7 @@ void buzz_stop();
 /* clear melody buffer */
 void buzz_clear();
 /* Note 1: volume should be less or equal to pitch */
-/* Note 2: Duration: 1 second equals approx to 3906 */
+/* Note 2: Duration: 1 second equals approx to ??? */
 uint8_t buzz_add_sound(uint8_t pitch, uint8_t volume, uint8_t duration);
 uint8_t buzz_add_pause(uint8_t duration);
 /* repeat melody num times */
