ee7131fe522e418294a6d07c49e20c9aaa19dda6
cppcoder123 <cppcoder123@vfemail.net>
2020-01-22 21:52:49 +0100

    avr: Handle 2 led matrices 32x8

--- src/avr/flush.c
+++ src/avr/flush.c
@@ -13,11 +13,12 @@
 #include "flush.h"
 
 #define MATRIX_SIZE 32
-/* #define POLL_LIMIT 64 /\* ? *\/ */
+#define MIN_DATA_SIZE (MATRIX_SIZE * 2)
 
 static volatile struct buffer_t led_data;
 
-static struct display_t display_1;
+static struct display_t display_right;
+static struct display_t display_left;
 
 enum {
   FLUSH_SHIFT,
@@ -47,7 +48,8 @@ void flush_init ()
   mode = FLUSH_DISABLED;
   global_mode = FLUSH_GLOBAL_DISABLED;
 
-  display_init (&display_1, PORTA2, PORTA6, PORTA4);
+  display_init (&display_left, PORTA0, PORTA2, PORTA4);
+  display_init (&display_right, PORTA1, PORTA3, PORTA5);
 }
 
 void flush_enable ()
@@ -85,22 +87,24 @@ void flush_try ()
 {
   if ((global_mode == FLUSH_GLOBAL_DISABLED)
       || (mode == FLUSH_DISABLED)
-      || (buffer_size (&led_data) < MATRIX_SIZE))
+      || (buffer_size (&led_data) < MIN_DATA_SIZE))
     return;
 
-  /* if (buffer_size (&led_data) < POLL_LIMIT) */
-  /*   encode_msg_1 (MSG_ID_POLL, SERIAL_ID_TO_IGNORE, flush_buffer_space ()); */
-
-  display_data_start (&display_1);
+  display_data_start (&display_left);
+  display_data_start (&display_right);
 
   uint8_t symbol;
-  for (uint8_t i = 0; i < MATRIX_SIZE; ++i)
-    if (buffer_byte_get (&led_data, i, &symbol) != 0)
-      display_data_column (&display_1, symbol);
-    else
-      display_data_column (&display_1, 0);
+  for (uint8_t i = 0; i < MATRIX_SIZE; ++i) {
+    if (buffer_byte_get (&led_data, i, &symbol) == 0)
+      symbol = 0;
+    display_data_column (&display_left, symbol);
+    if (buffer_byte_get (&led_data, i + MATRIX_SIZE, &symbol) == 0)
+      symbol = 0;
+    display_data_column (&display_right, symbol);
+  }
 
-  display_data_stop (&display_1);
+  display_data_stop (&display_left);
+  display_data_stop (&display_right);
 
   if (mode == FLUSH_SHIFT) {
     buffer_byte_drain (&led_data, &symbol);
