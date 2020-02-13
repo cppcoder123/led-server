47024f29b2a3632e7e0c67109317c6ba19e45f40
cppcoder123 <cppcoder123@vfemail.net>
2020-02-13 06:15:17 +0100

    avr: Add provoke limit to heartbeat

--- src/avr/heartbeat.c
+++ src/avr/heartbeat.c
@@ -5,6 +5,8 @@
 #include "invoke.h"
 #include "heartbeat.h"
 
+#define PROVOKE_LIMIT 1
+
 static uint8_t current_misses = 0;
 static uint8_t max_misses = 0;
 static heartbeat_callback report = 0;
@@ -31,7 +33,8 @@ static void verify ()
     return;
   }
 
-  provoke ();
+  if (current_misses > PROVOKE_LIMIT)
+    provoke ();
 }
 
 void heartbeat_start (uint8_t delay, uint8_t max,
@@ -42,7 +45,7 @@ void heartbeat_start (uint8_t delay, uint8_t max,
     return;
 
   current_misses = 0;
-  max_misses = max;
+  max_misses = PROVOKE_LIMIT + max;
   report = report_cb;
   provoke = provoke_cb;
 
