f52ed343cadc25318bfef3443df34ff77ee7c95e
cppcoder123 <cppcoder123@vfemail.net>
2020-02-15 15:03:39 +0100

    avr: Comment 'power', hw problem?

--- src/avr/power.c
+++ src/avr/power.c
@@ -4,39 +4,39 @@
 
 #include <avr/io.h>
 
-#include "at.h"
+/* #include "at.h" */
 #include "power.h"
 
-#define CONTROL_WIRE (1 << PORTE0)
+/* #define CONTROL_WIRE (1 << PORTE0) */
 
-#define OFF_DELAY 10
-#define HOLD_DELAY 60
+/* #define OFF_DELAY 10 */
+/* #define HOLD_DELAY 60 */
 
 void power_init ()
 {
   /* fixme: Configure power wire as output and set it to 0 */
 
   /* e0 => output  */
-  DDRE |= CONTROL_WIRE;
+  /* DDRE |= CONTROL_WIRE; */
 
   /* set it to 0 */
-  PORTE &= ~CONTROL_WIRE;
+  /* PORTE &= ~CONTROL_WIRE; */
 }
 
-static void raise_voltage ()
-{
-  PORTE &= CONTROL_WIRE;
-}
+/* static void raise_voltage () */
+/* { */
+/*   PORTE &= CONTROL_WIRE; */
+/* } */
 
-static void drop_voltage ()
-{
-  PORTE |= CONTROL_WIRE;
+/* static void drop_voltage () */
+/* { */
+/*   PORTE |= CONTROL_WIRE; */
 
-  at_schedule (AT_POWER, HOLD_DELAY, &raise_voltage);
-}
+/*   at_schedule (AT_POWER, HOLD_DELAY, &raise_voltage); */
+/* } */
 
 void power_on ()
 {
   return;                       /* hardware problem */
-  at_schedule (AT_POWER, OFF_DELAY, &drop_voltage);
+  /* at_schedule (AT_POWER, OFF_DELAY, &drop_voltage); */
 }
