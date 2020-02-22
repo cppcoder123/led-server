01a12ea0357142d90e3fce960197da3024aef4ff
cppcoder123 <cppcoder123@vfemail.net>
2020-02-22 15:38:14 +0100

    avr: Polish rotor

--- src/avr/rotor.c
+++ src/avr/rotor.c
@@ -41,7 +41,7 @@
 #define BOUNCE_1 (1 << 1)
 
 static volatile struct buf_t event_buf;
-static uint8_t toggled_bits = 0;
+static uint8_t old_event = 0;       /* toggled bits */
 static uint8_t bounce = 0;
 
 static rotor_callback callback = 0;
@@ -53,7 +53,7 @@ static void dummy_callback (uint8_t id, uint8_t action)
 void rotor_init ()
 {
   buf_init (&event_buf);
-  toggled_bits = 0;
+  old_event = 0;
   bounce = 0;
   callback = &dummy_callback;
   /* invoke_enable (INVOKE_ID_ROTOR, BOUNCE_DELAY, &bounce_clear); */
@@ -69,13 +69,6 @@ void rotor_init ()
 
   /*port K is input*/
   DDRK = 0;
-}
-
-static uint8_t apply_mask (uint8_t old, uint8_t new,
-                           uint8_t old_mask, uint8_t new_mask)
-{
-  return (((old & old_mask) == old_mask)
-          && ((new & new_mask) == new_mask)) ? 1 : 0;
 }
 
 static void bounce_clear_0 ()
@@ -105,36 +98,48 @@ static void debounce (uint8_t id)
   callback (id, ROTOR_PUSH);
 }
 
+static uint8_t apply_mask (uint8_t old, uint8_t new,
+                           uint8_t old_mask, uint8_t new_mask)
+{
+  return (((old & old_mask) == old_mask)
+          && ((new & new_mask) == new_mask)) ? 1 : 0;
+}
+
+static void handle_event (uint8_t event, uint8_t id)
+{
+  uint8_t mask_a = MASK_ROTOR_0_A;
+  uint8_t mask_b = MASK_ROTOR_0_B;
+  uint8_t mask_push = MASK_ROTOR_0_PUSH;
+
+  if (id != ROTOR_0) {
+    mask_a = MASK_ROTOR_1_A;
+    mask_b = MASK_ROTOR_1_B;
+    mask_push = MASK_ROTOR_1_PUSH;
+  }
+  const uint8_t mask_both = (mask_a | mask_b);
+
+  if (apply_mask (old_event, event, mask_a, mask_both) != 0)
+    callback (id, ROTOR_CLOCKWISE);
+  if (apply_mask (old_event, event, mask_b, mask_both) != 0)
+    callback (id, ROTOR_COUNTER_CLOCKWISE);
+  if (event & mask_push)
+    debounce (id);
+}
+
 void rotor_try ()
 {
   uint8_t src = 0;
   if (buf_byte_drain (&event_buf, &src) == 0)
     return;
 
-  /* negate src */
-  uint8_t new_toggled_bits = ~src;
+  uint8_t event = ~src;
 
   /* debug_1 (DEBUG_ROTOR, DEBUG_11, new_toggled_bits); */
 
-  if (apply_mask
-      (toggled_bits, new_toggled_bits, MASK_ROTOR_0_A, MASK_ROTOR_0_BOTH) != 0)
-    callback (ROTOR_0, ROTOR_CLOCKWISE);
-  if (apply_mask
-      (toggled_bits, new_toggled_bits, MASK_ROTOR_0_B, MASK_ROTOR_0_BOTH) != 0)
-    callback (ROTOR_0, ROTOR_COUNTER_CLOCKWISE);
-  if (new_toggled_bits & MASK_ROTOR_0_PUSH)
-    debounce (ROTOR_0);
+  handle_event (event, ROTOR_0);
+  handle_event (event, ROTOR_1);
 
-  if (apply_mask
-      (toggled_bits, new_toggled_bits, MASK_ROTOR_1_A, MASK_ROTOR_1_BOTH) != 0)
-    callback (ROTOR_1, ROTOR_COUNTER_CLOCKWISE);
-  if (apply_mask
-      (toggled_bits, new_toggled_bits, MASK_ROTOR_1_B, MASK_ROTOR_1_BOTH) != 0)
-    callback (ROTOR_1, ROTOR_CLOCKWISE);
-  if (new_toggled_bits & MASK_ROTOR_1_PUSH)
-    debounce (ROTOR_1);
-
-  toggled_bits = new_toggled_bits;
+  old_event = event;
 }
 
 ISR (PCINT2_vect)
