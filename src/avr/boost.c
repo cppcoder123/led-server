27f2e3297c78d04a0d340f77e8a365b7bb3ff452
cppcoder123 <cppcoder123@vfemail.net>
2020-03-12 20:43:27 +0100

    avr: Add code to boost

--- src/avr/boost.c
+++ src/avr/boost.c
@@ -3,6 +3,7 @@
  */
 
 #include "boost.h"
+#include "counter.h"
 #include "feedback.h"
 
 /* 12 volt, fixme check */
@@ -14,7 +15,42 @@
 /* delay , 5 times? fixme */
 #define DELAY 5
 
+#define BOOST_COUNTER COUNTER_3
+#define BOOST_PRESCALER COUNTER_PRESCALER_1
+/* 25 kHz */
+#define BOOST_COMPARE_A 160
+#define BOOST_PWM_MIN 3
+#define BOOST_PWM_MAX 157
+#define BOOST_PWM_MIDDLE ((BOOST_PWM_MIN + BOOST_PWM_MAX) / 2)
+#define BOOST_COMPARE_A_HIGH 0
+
 static struct feedback_t feedback;
+
+static void adc_start ()
+{
+  /* fixme */
+}
+
+static void adc_stop ()
+{
+  /* fixme */
+}
+
+static void counter_start ()
+{
+  /* fixme */
+  counter_enable (BOOST_COUNTER, BOOST_PRESCALER);
+  /* fixme: set flags in 'set_compare_a' and rename it to 'enable_compare_a' */
+  counter_set_compare_a (BOOST_COUNTER, BOOST_PWM_MIDDLE, BOOST_COMPARE_A_HIGH);
+}
+
+static void counter_stop ()
+{
+  /* fixme */
+  counter_disable (BOOST_COUNTER);
+  /* fixme */
+  /* counter_disable_compare_a (); */
+}
 
 static void control (uint8_t current)
 {
@@ -26,11 +62,15 @@ void boost_start ()
   /* init sw part */
   feedback_init (&feedback, TARGET, DELTA, DELAY, &control);
   /* init hw part, fixme */
+  counter_start ();
+  adc_start ();
 }
 
 void boost_stop ()
 {
   /* uninit hw part, fixme */
+  counter_stop ();
+  adc_stop ();
 }
 
 void boost_try ()
