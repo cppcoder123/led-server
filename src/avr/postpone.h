34560a46c89c5b4d56eee6d329535fbe72b3c55d
cppcoder123 <cppcoder123@vfemail.net>
2021-01-16 11:46:54 +0100

    avr: Edit comments

--- src/avr/postpone.h
+++ src/avr/postpone.h
@@ -1,5 +1,12 @@
 /*
- * fixme: Remove this module
+ * This is one message spi-write buffer.
+ *
+ * All messages can be split into 2 parts with SERIAL_ID_TO_IGNORE
+ * and serial id that can't be ignored. So, messages that have to
+ * be ignored can be send without delay. Messages with meaningfull
+ * id can be send to pi only we have enough space in avr queues:
+ * spi-write-queue, spi-read-queue, flush-queue
+ *
  */
 #ifndef POSTPONE_H
 #define POSTPONE_H
