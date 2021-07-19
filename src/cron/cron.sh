425c7125642af25b16171e7fb27b231223ff8be6
cppcoder123 <cppcoder123@vfemail.net>
2021-07-19 19:27:46 +0200

    cron: Restart led if needed

--- src/cron/cron.sh
+++ src/cron/cron.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+IS_RUNNING="0"
+status () {
+    sudo systemctl status led | egrep "active \(running\)" >& /dev/null
+    IS_RUNNING=$?
+}
+
+status ()
+
+if [! ${IS_RUNNING} -eq 0]
+then
+    sudo systemctl stop led
+    sudo systemctl start led
+fi
+
+status ()
+if [! ${IS_RUNNING} -eq 0]
+then
+    exit
+fi
+
 # check bt connection
 echo "info" | sudo bluetoothctl | grep "Connected: yes" >& /dev/null
 if [ ! $? -eq 0 ]
